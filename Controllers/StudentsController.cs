79c0747d28403e36fed18fab98939c5bdc734095
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-02-26 14:01:36 +0700

    Add files via upload

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using ett1_web_api.Models;
+using ett1_web_api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ett1_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentContext _context;
+
+        public StudentsController(StudentContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/Students/CreateStudent
+        [HttpPost("CreateStudent")]
+        public async Task<IActionResult> CreateStudent(Student student)
+        {
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Student added successfully", student });
+        }
+    }
+}
---
+++ Data/StudentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ett1_web_api.Models;
+
+namespace ett1_web_api.Data
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
